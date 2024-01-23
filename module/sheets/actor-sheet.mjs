99f92458c6e4963357ab49fe877c3dc733eb1263
Vlad Dracula <gmonlineua@gmail.com>
2024-01-23 13:38:06 +0200

    tunnel goons basic

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -184,7 +184,6 @@ export class BlankActorSheet extends ActorSheet
       let roll = new Roll(dataset.roll, this.actor.getRollData());
       roll.toMessage({
         speaker: ChatMessage.getSpeaker({ actor: this.actor }),
-        flavor: label,
         rollMode: game.settings.get('core', 'rollMode')
       });
       return roll;
